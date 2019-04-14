2f6bd29cc3a0c9fd38271c2fd0c8b3a7f5c4187d
Vruchtel <vruchtel.serafima@yandex.ru>
2019-04-14 23:57:57 +0300

    Add working processes and queue of tasks

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 import sys  # sys нужен для передачи argv в QApplication
 import os
+import time
+import threading
+from multiprocessing import Queue, Pool
+
 from PyQt5 import QtWidgets
 
+# TODO: 
+# контроль цпу - явный контроль количества запущенных процессов
+# контроль памяти - как-то спрашивать доступную память из питона
+
+# Класть результат в очередь результатов для того, чтобы отрисовывать результат
+# Для этого нужен отдельный ПОТОК, который слушает эту очередь
+# Ну и ещё нужно по имени файла научиться понимать, какой он по порядку в списке
+
 import design  # Это наш конвертированный файл дизайна
+
+tasks_queue = Queue()
+FINISH_TASK = "Finish"
+
+
+def worker_fun(queue):
+    while True:
+        task = queue.get()
+        print(task, str(time.time()))   
+        
+        if task == FINISH_TASK:
+            break
+    return
+            
+
 
 class ExampleApp(QtWidgets.QMainWindow, design.Ui_MyWowApp):
     def __init__(self):
@@ -11,23 +38,43 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MyWowApp):
         super().__init__()
         self.setupUi(self)  # Это нужно для инициализации нашего дизайна
         
-        self.deleteTaskButton.clicked.connect(self.browse_folder)
-        
-    def browse_folder(self):
-        self.listWidget.clear()  # На случай, если в списке уже есть элементы
-        directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Выберите папку")
-        # открыть диалог выбора директории и установить значение переменной
-        # равной пути к выбранной директории
+        self.addTaskButton.clicked.connect(self.upload_new_images)
+            
+            
+    def upload_new_images(self):
+        files = QtWidgets.QFileDialog.getOpenFileNames(self,
+                                     "Выберите один или несколько файлов",
+                                     "/home",
+                                     "Images (*.png *.xpm *.jpg)")
 
-        if directory:  # не продолжать выполнение, если пользователь не выбрал директорию
-            for file_name in os.listdir(directory):  # для каждого файла в директории
-                self.listWidget.addItem(file_name)   # добавить файл в listWidget
+        if files:
+            for file in files[0]:
+                self.tasksListWidget.addItem(file)
+                
+                tasks_queue.put(file)
+                print(tasks_queue.empty(), time.time())
+                
+                # Запустить счётчик % обработки
+                self.processListWidget.addItem("waiting")
+                
+                
+    def closeEvent(self, event):
+        for _ in range(PROCESSES_COUNT):
+            tasks_queue.put(FINISH_TASK)
+            print("Add finish task", tasks_queue.empty(), str(time.time()))
+        event.accept()
+                
         
-def main():
+def run_app():
     app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
     window = ExampleApp()  # Создаём объект класса ExampleApp
     window.show()  # Показываем окно
     app.exec_()  # и запускаем приложение
     
+    
+PROCESSES_COUNT = 4
+    
 if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
-    main()  # то запускаем функцию main()
+    with Pool(PROCESSES_COUNT, worker_fun, (tasks_queue,)) as workers_pool:
+        run_app()
+    
