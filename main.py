78604761fa67f3dca666efee9f24541a2f506a58
Vruchtel <vruchtel.serafima@yandex.ru>
2019-04-16 16:10:12 +0300

    Add queue with results

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import os
 import time
 import threading
 from multiprocessing import Queue, Pool
+import psutil
+
+from PIL import Image
 
 from PyQt5 import QtWidgets
 
@@ -10,23 +13,42 @@ from PyQt5 import QtWidgets
 # контроль цпу - явный контроль количества запущенных процессов
 # контроль памяти - как-то спрашивать доступную память из питона
 
-# Класть результат в очередь результатов для того, чтобы отрисовывать результат
-# Для этого нужен отдельный ПОТОК, который слушает эту очередь
-# Ну и ещё нужно по имени файла научиться понимать, какой он по порядку в списке
 
 import design  # Это наш конвертированный файл дизайна
 
 tasks_queue = Queue()
 FINISH_TASK = "Finish"
+PROCESSES_COUNT = 4
+
+results_queue = Queue()
 
 
-def worker_fun(queue):
+def worker_fun(tasks_queue, results_queue):
     while True:
-        task = queue.get()
-        print(task, str(time.time()))   
+        task = tasks_queue.get()
+#         print(task, str(time.time()))   
         
         if task == FINISH_TASK:
+            results_queue.put(task)
             break
+            
+        file_name = task.split(' ')[1]
+#         print(file_name)
+        
+        # Проверка того, есть ли доступная память
+        # Если доступная память есть - загрузка и обработка картинки
+        available_memory = psutil.virtual_memory().available  # объёмы доступной памяти в байтах
+        file_size = os.path.getsize(file_name)
+        
+        # Наверное, нужно проверку доступной памяти и считывание файла в память делать с помощью мьютекса
+        # Захватить мьютекс, спросить доступную память, считать файл, если достаточно, отпустить мьютекс
+        
+        print(available_memory, file_size)
+        image = Image.open(file_name)
+        
+        # Результат - в папку tmp по имени - имя файла
+            
+        results_queue.put(task)
     return
             
 
@@ -38,8 +60,12 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MyWowApp):
         super().__init__()
         self.setupUi(self)  # Это нужно для инициализации нашего дизайна
         
+        self.tasks_count = 0
         self.addTaskButton.clicked.connect(self.upload_new_images)
-            
+        
+        self.updating_result_thread = threading.Thread(target=self.update_result_info)
+        self.updating_result_thread.start()
+        
             
     def upload_new_images(self):
         files = QtWidgets.QFileDialog.getOpenFileNames(self,
@@ -49,21 +75,35 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MyWowApp):
 
         if files:
             for file in files[0]:
-                self.tasksListWidget.addItem(file)
+                self.tasks_count += 1
+                task_name = str(self.tasks_count) + ". " + file
+                self.tasksListWidget.addItem(task_name)
                 
-                tasks_queue.put(file)
+                tasks_queue.put(task_name)
                 print(tasks_queue.empty(), time.time())
                 
                 # Запустить счётчик % обработки
-                self.processListWidget.addItem("waiting")
+                self.processListWidget.addItem(str(self.tasks_count) + ". waiting")
                 
+                
+    def update_result_info(self):
+        while True:
+            task = results_queue.get()
+            if task == FINISH_TASK:
+                break
+                
+            task_id = task.split('.')[0]
+            
+            self.processListWidget.item(int(task_id) - 1).setText(task_id + ". ready")
+        return
+        
                 
     def closeEvent(self, event):
         for _ in range(PROCESSES_COUNT):
             tasks_queue.put(FINISH_TASK)
-            print("Add finish task", tasks_queue.empty(), str(time.time()))
         event.accept()
-                
+        
+        
         
 def run_app():
     app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
@@ -72,9 +112,8 @@ def run_app():
     app.exec_()  # и запускаем приложение
     
     
-PROCESSES_COUNT = 4
-    
 if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
-    with Pool(PROCESSES_COUNT, worker_fun, (tasks_queue,)) as workers_pool:
+    with Pool(PROCESSES_COUNT, worker_fun, (tasks_queue, results_queue,)) as workers_pool:
         run_app()
+    
     
